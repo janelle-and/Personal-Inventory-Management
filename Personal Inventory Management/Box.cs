a6b3b36d19f7e819a8748e7a84b8136e1782b8e4
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-03-22 13:40:32 -0600

    added the box class

---
+++ Personal Inventory Management/Box.cs
@@ -0,0 +1,10 @@
+﻿namespace Personal_Inventory_Management;
+public class Box {
+    public string Name { get; set; } // box name
+    public List<Tuple<string,Boolean>>? items {get; set;} // list of items in the box as well as a boolean (true or false) value for whether it is taken out or not. Each is a tuple so it will work something like this "[item:true,item:false,item:true] etc
+    /* Box object constructor */
+    public Box(string Name, List<Tuple<string,Boolean>>? items) {
+            this.Name = Name;
+            this.items = items ?? new List<Tuple<string,Boolean>>(); // question marks mean the list is nullable as a box can have no items in it
+    }
+}
