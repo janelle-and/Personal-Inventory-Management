0a2c29f0d5ee619eb2156298599dc07649865805
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-03-26 16:09:00 -0600

    get and display boxname

--- Personal Inventory Management/Form1.cs
+++ Personal Inventory Management/Form1.cs
@@ -15,26 +15,23 @@ namespace Personal_Inventory_Management
             fLayMainDisplay.Controls.Add(txt);
         }
 
-        //    private Control CreateBoxControl(Box box)
-        //    {
-        //        // Create a new Panel or any other Control to represent the Box
-        //        Panel panel = new Panel();
-        //        panel.Name = box.Name;
-        //        panel.Size = new Size(200, 200);
-        //        panel.BackColor = Color.LightGray;
-        //        PictureBox pictureBox = new PictureBox();
-        //        pictureBox.Name = "pictureBox";
-        //        pictureBox.Size = new Size(100, 100);
-        //        pictureBox.Location = new Point(50, 50);
-        //        // Add additional properties and child controls to the panel as needed
-
-        //        if (File.Exists(@"C:\\Users\\scemb\\OneDrive\\Pictures\\Random\\uppdatera-storage-box-light-bamboo__1029005_pe835616_s5.avi"))
-        //            pictureBox.Image = Image.FromFile(@"C:\\Users\\scemb\\OneDrive\\Pictures\\Random\\uppdatera-storage-box-light-bamboo__1029005_pe835616_s5.avi");
-
-        //        return panel;
-
-
-        //    }
+        private Control CreateBoxControl(Box box)
+        {
+            // Create a new Panel or any other Control to represent the Box
+            Panel panel = new Panel();
+            panel.Name = box.Name;
+            panel.Size = new Size(200, 200);
+            panel.BackColor = Color.LightGray;
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(100, 100);
+            pictureBox.Location = new Point(50, 50);
+            // Add additional properties and child controls to the panel as needed
+            string path = "Personal Inventory Management/project_Box_2.jpeg";
+            if (File.Exists(path))
+                pictureBox.Image = Image.FromFile(path);
+            return panel;
+        }
 
         //     private void GetCast(int id)
         //     {
