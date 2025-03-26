86a40d1c1abf9416a8d3834ae3e496284bb4c06f
thewhoshirt <s.embury.36@gmail.com>
2025-03-26 15:39:53 -0600

    Temp code to see if work

--- Personal Inventory Management/Form1.cs
+++ Personal Inventory Management/Form1.cs
@@ -1,34 +1,87 @@
 using Microsoft.VisualBasic.Devices;
+using System.Windows.Forms;
 
 namespace Personal_Inventory_Management
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        //    public Form1()
+        //    {
+        //        InitializeComponent();
+        //        Box Box1 = new Box("Box1", new List<Tuple<string, Boolean>>());
+        //        fLayMainDisplay.Controls.Add(CreateBoxControl(Box1));
+        //    }
+
+        //    private Control CreateBoxControl(Box box)
+        //    {
+        //        // Create a new Panel or any other Control to represent the Box
+        //        Panel panel = new Panel();
+        //        panel.Name = box.Name;
+        //        panel.Size = new Size(200, 200);
+        //        panel.BackColor = Color.LightGray;
+        //        PictureBox pictureBox = new PictureBox();
+        //        pictureBox.Name = "pictureBox";
+        //        pictureBox.Size = new Size(100, 100);
+        //        pictureBox.Location = new Point(50, 50);
+        //        // Add additional properties and child controls to the panel as needed
+
+        //        if (File.Exists(@"C:\\Users\\scemb\\OneDrive\\Pictures\\Random\\uppdatera-storage-box-light-bamboo__1029005_pe835616_s5.avi"))
+        //            pictureBox.Image = Image.FromFile(@"C:\\Users\\scemb\\OneDrive\\Pictures\\Random\\uppdatera-storage-box-light-bamboo__1029005_pe835616_s5.avi");
+
+        //        return panel;
+
+
+        //    }
+
+        private void GetCast(int id)
         {
-            InitializeComponent();
-            Box Box1 = new Box("Box1", new List<Tuple<string, Boolean>>());
-            fLayMainDisplay.Controls.Add(CreateBoxControl(Box1));
+            using (var context = new MoviesEntities())
+            {
+                var Movies = context.movielinkpersons.Where(p => p.movieId == id).ToList();
+                int i = 1;
+                foreach (var person in Movies)
+                {
+                    string Thumb = "";
+                    if (person.person.profile_path == "/JohnWayneBig.png")
+                    {
+                        Thumb = ImgPath + "JohnWayneSml.png";
+                        BigPicturePath = ImgPath + "JohnWayneBig.png";
+
+                    }
+                    else
+                    {
+                        Thumb = ImgURL + "w45" + person.person.profile_path;
+                        BigPicturePath = ImgURL + "original" + person.person.profile_path;
+                    }
+
+                    var picture = new PictureBox
+                    {
+                        Name = "pictureBox" + i.ToString(),
+                        Size = new Size(45, 70),
+
+
+                    };
+                    picture.Tag = BigPicturePath;
+                    picture.MouseHover += ThumbPictureBox_MouseHover;
+                    picture.LoadAsync(Thumb);
+                    flowLayoutPanel1.Controls.Add(picture);
+                    i++;
+                }
+
+
+            }
         }
 
-        private Control CreateBoxControl(Box box)
+        private void ThumbPictureBox_MouseHover(object sender, EventArgs e)
         {
-            // Create a new Panel or any other Control to represent the Box
-            Panel panel = new Panel();
-            panel.Name = box.Name;
-            panel.Size = new Size(200, 200);
-            panel.BackColor = Color.LightGray;
-            PictureBox pictureBox = new PictureBox();
-            pictureBox.Name = "pictureBox";
-            pictureBox.Size = new Size(100, 100);
-            pictureBox.Location = new Point(50, 50);
-            // Add additional properties and child controls to the panel as needed
-
-            if (File.Exists(@"C:\\Users\\scemb\\OneDrive\\Pictures\\Random\\uppdatera-storage-box-light-bamboo__1029005_pe835616_s5.avi"))
-                pictureBox.Image = Image.FromFile(@"C:\\Users\\scemb\\OneDrive\\Pictures\\Random\\uppdatera-storage-box-light-bamboo__1029005_pe835616_s5.avi");
-            
-            return panel;
-
+            //BigPicturePath = this.Tag.ToString();
+            if (BigPicturePath != null)
+            {
+                string Orientation = "P";
+                FilmsDB.PictureForm frm = new PictureForm(Orientation);
+                frm.SetValues(BigPicturePath);
+                frm.ShowDialog();
+            }
 
         }
     }
