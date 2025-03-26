4abade293b844485dd17f1bbc5e767cea21cc5c6
thewhoshirt <s.embury.36@gmail.com>
2025-03-26 15:28:26 -0600

    Box be there

--- Personal Inventory Management/Form1.Designer.cs
+++ Personal Inventory Management/Form1.Designer.cs
@@ -28,55 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            propertyGrid1 = new PropertyGrid();
-            pictureBox1 = new PictureBox();
-            label1 = new Label();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            fLayMainDisplay = new FlowLayoutPanel();
+            groupBox1 = new GroupBox();
             SuspendLayout();
             // 
-            // propertyGrid1
+            // fLayMainDisplay
             // 
-            propertyGrid1.Location = new Point(12, 12);
-            propertyGrid1.Name = "propertyGrid1";
-            propertyGrid1.Size = new Size(1185, 361);
-            propertyGrid1.TabIndex = 0;
+            fLayMainDisplay.Location = new Point(64, 86);
+            fLayMainDisplay.Name = "fLayMainDisplay";
+            fLayMainDisplay.Size = new Size(573, 499);
+            fLayMainDisplay.TabIndex = 3;
             // 
-            // pictureBox1
+            // groupBox1
             // 
-            pictureBox1.Location = new Point(39, 55);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(123, 131);
-            pictureBox1.TabIndex = 1;
-            pictureBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(73, 201);
-            label1.Name = "label1";
-            label1.Size = new Size(50, 20);
-            label1.TabIndex = 2;
-            label1.Text = "label1";
+            groupBox1.Location = new Point(713, 77);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(386, 382);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1217, 665);
-            Controls.Add(label1);
-            Controls.Add(pictureBox1);
-            Controls.Add(propertyGrid1);
+            Controls.Add(groupBox1);
+            Controls.Add(fLayMainDisplay);
             Name = "Form1";
             Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private PropertyGrid propertyGrid1;
-        private PictureBox pictureBox1;
-        private Label label1;
+        private FlowLayoutPanel fLayMainDisplay;
+        private GroupBox groupBox1;
     }
 }
--- Personal Inventory Management/Form1.cs
+++ Personal Inventory Management/Form1.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic.Devices;
+
 namespace Personal_Inventory_Management
 {
     public partial class Form1 : Form
@@ -5,8 +7,29 @@ namespace Personal_Inventory_Management
         public Form1()
         {
             InitializeComponent();
+            Box Box1 = new Box("Box1", new List<Tuple<string, Boolean>>());
+            fLayMainDisplay.Controls.Add(CreateBoxControl(Box1));
         }
 
-        
+        private Control CreateBoxControl(Box box)
+        {
+            // Create a new Panel or any other Control to represent the Box
+            Panel panel = new Panel();
+            panel.Name = box.Name;
+            panel.Size = new Size(200, 200);
+            panel.BackColor = Color.LightGray;
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(100, 100);
+            pictureBox.Location = new Point(50, 50);
+            // Add additional properties and child controls to the panel as needed
+
+            if (File.Exists(@"C:\\Users\\scemb\\OneDrive\\Pictures\\Random\\uppdatera-storage-box-light-bamboo__1029005_pe835616_s5.avi"))
+                pictureBox.Image = Image.FromFile(@"C:\\Users\\scemb\\OneDrive\\Pictures\\Random\\uppdatera-storage-box-light-bamboo__1029005_pe835616_s5.avi");
+            
+            return panel;
+
+
+        }
     }
 }
