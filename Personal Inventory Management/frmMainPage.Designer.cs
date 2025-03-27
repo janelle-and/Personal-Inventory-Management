52727b3837ab6bd3b10d5665c720eba594d84762
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-03-27 09:39:48 -0600

    box name and pic bound together

--- Personal Inventory Management/frmMainPage.Designer.cs
+++ Personal Inventory Management/frmMainPage.Designer.cs
@@ -23,82 +23,83 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            fLayMainDisplay = new FlowLayoutPanel();
-            textBox1 = new TextBox();
-            label1 = new Label();
-            btnAdd = new Button();
-            btnExit = new Button();
-            lblSearchResult = new Label();
+            fLayMainDisplay = new System.Windows.Forms.FlowLayoutPanel();
+            textBox1 = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            btnAdd = new System.Windows.Forms.Button();
+            btnExit = new System.Windows.Forms.Button();
+            lblSearchResult = new System.Windows.Forms.Label();
             SuspendLayout();
             // 
             // fLayMainDisplay
             // 
             fLayMainDisplay.AutoScroll = true;
-            fLayMainDisplay.Location = new Point(85, 86);
+            fLayMainDisplay.Location = new System.Drawing.Point(85, 86);
             fLayMainDisplay.Name = "fLayMainDisplay";
-            fLayMainDisplay.Size = new Size(1058, 359);
+            fLayMainDisplay.Size = new System.Drawing.Size(1058, 359);
             fLayMainDisplay.TabIndex = 3;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(925, 488);
+            textBox1.Location = new System.Drawing.Point(925, 488);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(199, 27);
+            textBox1.Size = new System.Drawing.Size(199, 23);
             textBox1.TabIndex = 4;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(863, 491);
+            label1.Location = new System.Drawing.Point(863, 491);
             label1.Name = "label1";
-            label1.Size = new Size(56, 20);
+            label1.Size = new System.Drawing.Size(45, 15);
             label1.TabIndex = 5;
             label1.Text = "Search:";
             // 
             // btnAdd
             // 
-            btnAdd.Location = new Point(58, 528);
+            btnAdd.Location = new System.Drawing.Point(58, 528);
             btnAdd.Name = "btnAdd";
-            btnAdd.Size = new Size(94, 29);
+            btnAdd.Size = new System.Drawing.Size(94, 29);
             btnAdd.TabIndex = 6;
             btnAdd.Text = "&Add";
             btnAdd.UseVisualStyleBackColor = true;
             // 
             // btnExit
             // 
-            btnExit.Location = new Point(215, 528);
+            btnExit.Location = new System.Drawing.Point(215, 528);
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(94, 29);
+            btnExit.Size = new System.Drawing.Size(94, 29);
             btnExit.TabIndex = 7;
             btnExit.Text = "E&xit";
             btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
             // 
             // lblSearchResult
             // 
             lblSearchResult.AutoSize = true;
-            lblSearchResult.Location = new Point(863, 547);
+            lblSearchResult.Location = new System.Drawing.Point(863, 547);
             lblSearchResult.Name = "lblSearchResult";
-            lblSearchResult.Size = new Size(52, 20);
+            lblSearchResult.Size = new System.Drawing.Size(42, 15);
             lblSearchResult.TabIndex = 8;
             lblSearchResult.Text = "Result:";
             // 
             // frmMainPage
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1217, 665);
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1217, 665);
             Controls.Add(lblSearchResult);
             Controls.Add(btnExit);
             Controls.Add(btnAdd);
             Controls.Add(label1);
             Controls.Add(textBox1);
             Controls.Add(fLayMainDisplay);
-            Name = "frmMainPage";
+            Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             Text = "Personal Inventory Management System";
             ResumeLayout(false);
             PerformLayout();
@@ -110,7 +111,7 @@
         private TextBox textBox1;
         private Label label1;
         private Button btnAdd;
-        private Button btnExit;
+        private System.Windows.Forms.Button btnExit;
         private Label lblSearchResult;
     }
 }
--- Personal Inventory Management/frmMainPage.cs
+++ Personal Inventory Management/frmMainPage.cs
@@ -9,23 +9,26 @@ namespace Personal_Inventory_Management
         {
             InitializeComponent();
             Box Box1 = new Box("Box1", new List<Tuple<string, Boolean>>());
-            Label txt = new Label();
-            txt.Name = "BoxName";
-            txt.Text = Box1.Name;
-            fLayMainDisplay.Controls.Add(CreateBoxControl(Box1));
+            string txt = Box1.Name;
+            fLayMainDisplay.Controls.Add(CreateBoxControl(Box1,txt));
         }
 
-        private Control CreateBoxControl(Box box)
+        private Control CreateBoxControl(Box box,String boxName)
         {
             // Create a new Panel or any other Control to represent the Box
             Panel panel = new Panel();
             panel.Name = box.Name;
-            panel.Size = new Size(200, 200);
+            panel.Size = new Size(100, 150);
             panel.BackColor = Color.LightGray;
+            Label boxTitle = new Label();
+            boxTitle.Text = boxName;
+            boxTitle.Dock = DockStyle.Bottom;
+            panel.Controls.Add(boxTitle);
             PictureBox pictureBox = new PictureBox();
             pictureBox.Name = "pictureBox";
-            pictureBox.Size = new Size(200,200);
-            pictureBox.Location = new Point(0, 10);
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox.Size = new Size(100,100);
+            pictureBox.Location = new Point(0, 0);
             // Add additional properties and child controls to the panel as needed
             string path = "../../../project_Box_2.jpeg";
             if (File.Exists(path))
@@ -34,57 +37,9 @@ namespace Personal_Inventory_Management
             return panel;
         }
 
-        //     private void GetCast(int id)
-        //     {
-        //         using (var context = new MoviesEntities())
-        //         {
-        //             var Movies = context.movielinkpersons.Where(p => p.movieId == id).ToList();
-        //             int i = 1;
-        //             foreach (var person in Movies)
-        //             {
-        //                 string Thumb = "";
-        //                 if (person.person.profile_path == "/JohnWayneBig.png")
-        //                 {
-        //                     Thumb = ImgPath + "JohnWayneSml.png";
-        //                     BigPicturePath = ImgPath + "JohnWayneBig.png";
-        //
-        //                 }
-        //                 else
-        //                 {
-        //                     Thumb = ImgURL + "w45" + person.person.profile_path;
-        //                     BigPicturePath = ImgURL + "original" + person.person.profile_path;
-        //                 }
-        //
-        //                 var picture = new PictureBox
-        //                 {
-        //                     Name = "pictureBox" + i.ToString(),
-        //                     Size = new Size(45, 70),
-        //
-        //
-        //                 };
-        //                 picture.Tag = BigPicturePath;
-        //                 picture.MouseHover += ThumbPictureBox_MouseHover;
-        //                 picture.LoadAsync(Thumb);
-        //                 flowLayoutPanel1.Controls.Add(picture);
-        //                 i++;
-        //             }
-        //
-        //
-        //         }
-        //     }
-        //
-        //     private void ThumbPictureBox_MouseHover(object sender, EventArgs e)
-        //     {
-        //         //BigPicturePath = this.Tag.ToString();
-        //         if (BigPicturePath != null)
-        //         {
-        //             string Orientation = "P";
-        //             FilmsDB.PictureForm frm = new PictureForm(Orientation);
-        //             frm.SetValues(BigPicturePath);
-        //             frm.ShowDialog();
-        //         }
-        //
-        //     }
-        // }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
