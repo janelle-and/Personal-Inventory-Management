ff3804d07fb9522a808a5432426a8397a1a76f0d
thewhoshirt <s.embury.36@gmail.com>
2025-03-30 13:31:44 -0600

    Search button added

--- Personal Inventory Management/frmMainPage.Designer.cs
+++ Personal Inventory Management/frmMainPage.Designer.cs
@@ -28,45 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            fLayMainDisplay = new System.Windows.Forms.FlowLayoutPanel();
-            textBox1 = new System.Windows.Forms.TextBox();
-            label1 = new System.Windows.Forms.Label();
-            btnAdd = new System.Windows.Forms.Button();
-            btnExit = new System.Windows.Forms.Button();
-            lblSearchResult = new System.Windows.Forms.Label();
-            lblResult = new System.Windows.Forms.Label();
+            fLayMainDisplay = new FlowLayoutPanel();
+            textBox1 = new TextBox();
+            btnAdd = new Button();
+            btnExit = new Button();
+            lblSearchResult = new Label();
+            lblResult = new Label();
+            btnSearch = new Button();
             SuspendLayout();
             // 
             // fLayMainDisplay
             // 
             fLayMainDisplay.AutoScroll = true;
-            fLayMainDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            fLayMainDisplay.Location = new System.Drawing.Point(12, 12);
+            fLayMainDisplay.BorderStyle = BorderStyle.FixedSingle;
+            fLayMainDisplay.Location = new Point(14, 16);
+            fLayMainDisplay.Margin = new Padding(3, 4, 3, 4);
             fLayMainDisplay.Name = "fLayMainDisplay";
-            fLayMainDisplay.Size = new System.Drawing.Size(1058, 359);
+            fLayMainDisplay.Size = new Size(1209, 478);
             fLayMainDisplay.TabIndex = 3;
             // 
             // textBox1
             // 
-            textBox1.Location = new System.Drawing.Point(871, 421);
+            textBox1.Location = new Point(995, 561);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new System.Drawing.Size(199, 23);
+            textBox1.Size = new Size(227, 27);
             textBox1.TabIndex = 4;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(790, 424);
-            label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(45, 15);
-            label1.TabIndex = 5;
-            label1.Text = "Search:";
             // 
             // btnAdd
             // 
-            btnAdd.Location = new System.Drawing.Point(12, 417);
+            btnAdd.Location = new Point(14, 556);
+            btnAdd.Margin = new Padding(3, 4, 3, 4);
             btnAdd.Name = "btnAdd";
-            btnAdd.Size = new System.Drawing.Size(94, 29);
+            btnAdd.Size = new Size(107, 39);
             btnAdd.TabIndex = 6;
             btnAdd.Text = "&Add";
             btnAdd.UseVisualStyleBackColor = true;
@@ -74,9 +68,10 @@
             // 
             // btnExit
             // 
-            btnExit.Location = new System.Drawing.Point(138, 417);
+            btnExit.Location = new Point(158, 556);
+            btnExit.Margin = new Padding(3, 4, 3, 4);
             btnExit.Name = "btnExit";
-            btnExit.Size = new System.Drawing.Size(94, 29);
+            btnExit.Size = new Size(107, 39);
             btnExit.TabIndex = 7;
             btnExit.Text = "E&xit";
             btnExit.UseVisualStyleBackColor = true;
@@ -85,33 +80,42 @@
             // lblSearchResult
             // 
             lblSearchResult.AutoSize = true;
-            lblSearchResult.Location = new System.Drawing.Point(790, 473);
+            lblSearchResult.Location = new Point(903, 631);
             lblSearchResult.Name = "lblSearchResult";
-            lblSearchResult.Size = new System.Drawing.Size(42, 15);
+            lblSearchResult.Size = new Size(52, 20);
             lblSearchResult.TabIndex = 8;
             lblSearchResult.Text = "Result:";
             // 
             // lblResult
             // 
-            lblResult.Location = new System.Drawing.Point(871, 473);
+            lblResult.Location = new Point(995, 631);
             lblResult.Name = "lblResult";
-            lblResult.Size = new System.Drawing.Size(199, 23);
+            lblResult.Size = new Size(227, 31);
             lblResult.TabIndex = 9;
             lblResult.Text = "This is some placeholder text";
             // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(882, 561);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(94, 29);
+            btnSearch.TabIndex = 10;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            // 
             // frmMainPage
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(1091, 535);
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1247, 713);
+            Controls.Add(btnSearch);
             Controls.Add(lblResult);
             Controls.Add(lblSearchResult);
             Controls.Add(btnExit);
             Controls.Add(btnAdd);
-            Controls.Add(label1);
             Controls.Add(textBox1);
             Controls.Add(fLayMainDisplay);
-            Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            Name = "frmMainPage";
             Text = "Personal Inventory Management System";
             ResumeLayout(false);
             PerformLayout();
@@ -123,9 +127,9 @@
 
         private System.Windows.Forms.FlowLayoutPanel fLayMainDisplay;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Label lblSearchResult;
+        private Button btnSearch;
     }
 }
